37b60a85b79504e82e7a092261496fb4cf4d2809
Joanne Badua <joannebadua@gmail.com>
2019-06-20 17:12:49 -0700

    spotify-this-song and four requirements

--- liri.js
+++ liri.js
@@ -9,9 +9,9 @@ var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 
 console.log(process.argv, "what we type in terminal");
-var commands = process.argv[2]
+var command = process.argv[2]
 var userInput = process.argv[3]
-console.log(commands, userInput)
+console.log(command, userInput)
 
 
 function liribotWork () {
@@ -34,35 +34,40 @@ liribotWork ();
 
 function spotifyStuff () {
   spotify
-  .search({ type: 'track', query: 'All the Small Things', limit: 5})
+  // .search({ type: 'track', query: 'All the Small Things', limit: 5})
+  .search({ type: 'track', query: userInput, limit: 5})
   .then(function(response) {
-    console.log(response.tracks);
+    console.log("Click link to sample song", response.tracks.items[0].album.external_urls);
+    console.log("The artist is: " , response.tracks.items[0].album.artists[0].name);
+    console.log("The song name is: " + response.tracks.items[0].name);
+    // console.log("Click here to sample", response.tracks.items[0].external_urls);
+    console.log("The song is from this album: " , response.tracks.items[0].album.name);
   })
-  .catch(function(err) {
-    // console.log(err); this is a test
-    if(error.response){
-      console.log("~~~~~~Data~~~~");
-      console.log(error.response.data);
-      console.log("~~~~Status~~~");
-      console.log(error.response.status);
-      console.log("~~~~Headers~~~");
-      console.log(error.response.headers);
-    } else if (error.resquest) {
-      console.log(error.request);
-    } else {
-      console.log("Error", error.message);
-    }
-    console.log(error.config);
-    }
-  )};
-
+  // .catch(function(err) {
+  //   // console.log(err); this is a test
+  //   if(error.response){
+  //     console.log("~~~~~~Data~~~~");
+  //     console.log(error.response.data);
+  //     console.log("~~~~Status~~~");
+  //     console.log(error.response.status);
+  //     console.log("~~~~Headers~~~");
+  //     console.log(error.response.headers);
+  //   } else if (error.resquest) {
+  //     console.log(error.request);
+  //   } else {
+  //     console.log("Error", error.message);
+  //   }
+  //   console.log(error.config);
+  //   }
+  // )};
+};
 function bandStuff () {
 axios.get("https://rest.bandsintown.com/artists/" + "digablePlanets" + "/events?app_id=codingbootcamp")
 
 .then (
   function(response){
     // console.log("what we got back from spotify", response. data);
-    console.log("what we got back from spotify", response.data[0].venue.name);
+    console.log("what we got back from bandsintown", response.data[0].venue.name);
   }
 )
 .catch(function(error){
